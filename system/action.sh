e1ce4c60cf5e0d1a9491ebc3e858045711dcc3e0
OnePlus Slave <pyellow46@gmail.com>
2024-08-12 18:05:56 +0300

    Update action.sh

--- system/action.sh
+++ system/action.sh
@@ -4,4 +4,6 @@ ln -s /etc/sddm.conf /etc/sddm.conf.d/autologin.conf
 wget https://otval228.github.io/methaneos_repo/system/usr/bin/steamos-session-select
 mv steamos-session-select /usr/bin/steamos-session-select
 chmod +x /usr/bin/steamos-session-select
+mkdir /etc/updater
+touch /etc/updater/update_hash
 echo "Done!"
