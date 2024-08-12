cef3f69727bfb81e7220eb51a770759cb97e8523
OnePlus Slave <pyellow46@gmail.com>
2024-08-12 16:41:03 +0300

    up

--- system/action.sh
+++ system/action.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-wget https://otval228.github.io/methaneos_repo/system/etc/updater/dummy
-mv dummy /etc/updater/dummy
+sudo systemctl enable fstrim.timer
+ln -s /etc/sddm.conf /etc/sddm.conf.d/autologin.conf
+wget https://otval228.github.io/methaneos_repo/system/usr/bin/steamos-session-select
+mv steamos-session-select /usr/bin/steamos-session-select
+chmod +x /usr/bin/steamos-session-select
 echo "Done!"
