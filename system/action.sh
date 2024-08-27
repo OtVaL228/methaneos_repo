33ae0ae24ca7f242cdb9b2eeef82d10c7029e843
OnePlus Slave <pyellow46@gmail.com>
2024-08-27 19:06:17 +0300

    Big update

--- system/action.sh
+++ system/action.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-sudo systemctl enable fstrim.timer
-ln -s /etc/sddm.conf /etc/sddm.conf.d/autologin.conf
-wget https://otval228.github.io/methaneos_repo/system/usr/bin/steamos-session-select
-mv steamos-session-select /usr/bin/steamos-session-select
-chmod +x /usr/bin/steamos-session-select
-mkdir /etc/updater
-touch /etc/updater/update_hash
+sudo systemctl enable pcscd.service
+wget https://otval228.github.io/methaneos_repo/system/usr/bin/methaneos-sessions
+wget https://otval228.github.io/methaneos_repo/system/etc/systemd/system/methaneos-update.service
+wget https://otval228.github.io/methaneos_repo/system/etc/systemd/system/methaneos-update.timer
+mv methaneos-sessions /usr/bin/
+chmod +x /usr/bin/methaneos-sessions
+mv methaneos-update.service /etc/systemd/system/
+mv methaneos-update.timer /etc/systemd/system/
+sudo systemctl enable methaneos-update.timer
+sudo systemctl enable methaneos-update.service
+bash /usr/bin/methaneos-sessions
 echo "Done!"
