22faf937157625cfd674724318ec71a0857c25f5
OnePlus Slave <pyellow46@gmail.com>
2024-08-29 23:46:43 +0300

    111

---
+++ versions/2024.06.12/system/action.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://otval228.github.io/methaneos_repo/versions/2024.06.12/etc/Updater/World
+mv World /etc/Updater/World
+echo "Done!"
--- versions/2024.08.12/system/action.sh
+++ versions/2024.08.12/system/action.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo systemctl enable fstrim.timer
 ln -s /etc/sddm.conf /etc/sddm.conf.d/autologin.conf
-wget https://otval228.github.io/methaneos_repo/system/usr/bin/steamos-session-select
+wget https://otval228.github.io/methaneos_repo/versions/2024.08.12/system/usr/bin/steamos-session-select
 mv steamos-session-select /usr/bin/steamos-session-select
 chmod +x /usr/bin/steamos-session-select
 mkdir /etc/updater
